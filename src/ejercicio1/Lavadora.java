16cf7f7ab63107d47350c762ba32c1a250203e94
eilmadc@gmail.com <eilmadc@gmail.com>
2023-03-15 10:51:12 +0100

    Create Lavadora.java

---
+++ src/ejercicio1/Lavadora.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package ejercicio1;
+
+/**
+ * @author Team01 (Elena, Palmira, Paul)
+ *
+ */
+public class Lavadora extends Electrodomestico {
+
+	final static double CARGA = 5;
+	private double carga;
+
+	/**
+	 * Constructor sin parametros por defecto
+	 * 
+	 * @param
+	 */
+	public Lavadora() {
+		this.carga = CARGA; // 5
+	}
+
+	/**
+	 * Constructor sin parametros por defecto
+	 * 
+	 * @param
+	 */
+	public Lavadora(double precioBase, double peso) {
+		super(precioBase, peso);
+		this.carga = CARGA; // 5
+	}
+
+	/**
+	 * Constructor parametros
+	 * 
+	 * @param precioBase,color, consumoEnergetico, peso
+	 */
+	public Lavadora(double precioBase, String color, char consumoEnergetico, double peso, double carga) {
+		super(precioBase, color, consumoEnergetico, peso);
+		this.carga = carga; 
+	}
+
+	/**
+	 * Obtener carga Getter
+	 * @return carga
+	 */
+	public double  getCarga() {
+		return carga;
+	}
+
+	public void precioFinal() {
+		if (carga>30) {
+			this.precioBase
+		}
+	}
+	
+}
