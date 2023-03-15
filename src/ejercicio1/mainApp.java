a0221a94cbf132b5817540a38b5c867996ecf55c
Paul S <62121921+paul-23@users.noreply.github.com>
2023-03-15 12:22:10 +0100

    Solucion archivos JDK

--- src/ejercicio1/mainApp.java
+++ src/ejercicio1/mainApp.java
@@ -13,7 +13,8 @@ public class mainApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Electrodomestico elec1 = new Electrodomestico();
+		System.out.println(elec1);
 
 	}
 
