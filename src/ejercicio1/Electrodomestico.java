2b06fd91fa17091683d29fa391782ee3765c309b
eilmadc@gmail.com <eilmadc@gmail.com>
2023-03-15 10:01:04 +0100

    crear Ejercicio 1

---
+++ src/ejercicio1/Electrodomestico.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package ejercicio1;
+
+/**
+ * @author elena-01
+ *
+ */
+public class Electrodomestico {
+
+}
---
+++ src/ejercicio1/mainApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ejercicio1;
+
+/**
+ * @author elena-01
+ *
+ */
+public class mainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
