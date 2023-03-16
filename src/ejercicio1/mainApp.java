f5dd9bb487e8ad1088341790a2328ea22bb51ab8
Paul S <62121921+paul-23@users.noreply.github.com>
2023-03-16 22:00:14 +0100

    Añadir comentarios de código

--- src/ejercicio1/mainApp.java
+++ src/ejercicio1/mainApp.java
@@ -14,6 +14,7 @@ public class mainApp {
 	 */
 	public static void main(String[] args) {
 
+		// Imprimir linas  para separar la cabecera del programa
 		System.out.println("\n****************************************");
 		System.out.println("************** C02 * UD09 **************");
 		System.out.println("****************************************\n");
@@ -21,13 +22,16 @@ public class mainApp {
 		// C2-T06: Ejercicio 1
 		System.out.println("************** Ejercicio 1 *************\n");
 
+		// Se crea un array de objetos Electrodomestico y se le asignan valores usando el método rellenarDatos.
 		Electrodomestico electrodomestico[] = new Electrodomestico[10];
-
 		rellenarDatos(electrodomestico);
+		
+		// Se muestran y calculan los datos usando el método mostrarCalcularDatos.
 		mostrarCalcularDatos(electrodomestico);
 
 	}
 
+	// Este método se encarga de rellenar el array de objetos Electrodomestico con datos
 	public static void rellenarDatos(Electrodomestico electrodomestico[]) {
 		electrodomestico[0] = new Electrodomestico(750, "rojo", 'E', 45);
 		electrodomestico[1] = new Electrodomestico(550, "negro", 'A', 25);
@@ -41,11 +45,14 @@ public class mainApp {
 		electrodomestico[9] = new Television(320, 22, 'J', "morado", 720, false);
 	}
 
+	// Mostrar los datos y de calcular el precio total de cada tipo de electrodoméstico
 	public static void mostrarCalcularDatos(Electrodomestico electrodomestico[]) {
 
 		double totalElectrodomesticos = 0, totalTelevisores = 0, totalLavadoras = 0;
 
 		System.out.println("************** Listado de productos **************");
+		
+		// Recorre el array de electrodomésticos y suma el total segun el tipo que sea.
 		for (int i = 0; i < electrodomestico.length; i++) {
 
 			if (electrodomestico[i] instanceof Electrodomestico) {
@@ -57,13 +64,14 @@ public class mainApp {
 			if (electrodomestico[i] instanceof Television) {
 				totalTelevisores = totalTelevisores + electrodomestico[i].precioFinal();
 			}
-
+			// Muestra por pantalla todos los datos
 			System.out.println((i + 1) + ": " + electrodomestico[i].toString());
 		}
 
 		mostrarTotal(totalElectrodomesticos, totalTelevisores, totalLavadoras);
 	}
-
+	
+	//Mostramos por pantalla la suma total de los precios
 	public static void mostrarTotal(double totalElectrodomesticos, double totalTelevisores, double totalLavadoras) {
 		System.out.println(
 				"\n************** TOTAL **************" + "\n- Total electrodomésticos: " + totalElectrodomesticos
