856a3a847d695b926f4f6d65577e22b03ce59fbc
Palmira Romia <100615218+Laurelindorean@users.noreply.github.com>
2023-03-19 20:24:35 +0100

    Implementacion de comentarios en ejercicio3

--- src/ejercicio3/Libro.java
+++ src/ejercicio3/Libro.java
@@ -9,27 +9,29 @@ package ejercicio3;
  */
 public class Libro {
 	
+	//-------------------- VARIABLES -----------------------
 	private int isbn;
 	private String titulo;
 	private String autor;
 	private int numPaginas;
 	
 	
+	//------------------- CONSTRUCTORES --------------------
 	
-	
+	// Constructor por defecto
 	public Libro() {
 		
 	}
+	//Constructor que recibe todos los parametros
 	public Libro(int isbn, String titulo, String autor, int numPaginas) {
 		this.isbn = isbn;
 		this.titulo = titulo;
 		this.autor = autor;
 		this.numPaginas = numPaginas;
 	}
-	@Override
-	public String toString() {
-		return "El libro " + titulo + " con ISBN " + isbn + ", creado por el autor " + autor + " tiene " + numPaginas + " páginas.";
-	}
+	
+	//Setters y Getters
+	
 	public int getIsbn() {
 		return isbn;
 	}
@@ -53,6 +55,12 @@ public class Libro {
 	}
 	public void setNumPaginas(int numPaginas) {
 		this.numPaginas = numPaginas;
+	}
+	
+	//Sobreescribimos el metodo toString
+	@Override
+	public String toString() {
+		return "El libro " + titulo + " con ISBN " + isbn + ", creado por el autor " + autor + " tiene " + numPaginas + " páginas.";
 	}
 	
 
