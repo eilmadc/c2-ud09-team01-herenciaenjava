3d6be065c3f5072dbd0e58d00c8660d19524a982
Palmira Romia <100615218+Laurelindorean@users.noreply.github.com>
2023-03-19 20:19:43 +0100

    Actualizacion de la clase Television con comentarios

--- src/ejercicio1/Television.java
+++ src/ejercicio1/Television.java
@@ -7,42 +7,50 @@ package ejercicio1;
  * @author Team01 (Elena, Palmira, Paul)
  *
  */
-public class Television extends Electrodomestico{
-	
+public class Television extends Electrodomestico {
+
+	//------------ CONSTANTES ---------------
 	private static final int RESOLUCION_DEF = 20;
 	private static boolean SINTONIZADOR_DEF = false;
-	
+	//------------ VARIABLES ----------------
 	private int resolucion;
 	private boolean sintonizadorTDT;
+
+	// ------------ CONSTRUCTORES------------
 
 	// Constructor por defecto
 	public Television() {
 		this(PRC_BSE, PSO_BSE, CNSMO_ENRG, COLOR_BSE, RESOLUCION_DEF, SINTONIZADOR_DEF);
 	}
 
+	// Constructor con 2 parametros de entrada y el resto por defecto
 	public Television(double precioBase, double peso) {
 		this(precioBase, peso, CNSMO_ENRG, COLOR_BSE, RESOLUCION_DEF, SINTONIZADOR_DEF);
 	}
 
+	// Constructor con todos los parametros
 	public Television(double precioBase, double peso, char consumoEnergetico, String color, int resolucion,
 			boolean sintonizadorTDT) {
 		super(precioBase, color, consumoEnergetico, peso);
 		this.resolucion = resolucion;
 		this.sintonizadorTDT = sintonizadorTDT;
 	}
-	
-	
+
+	// Metodo para calcular el precio final
 	public double precioFinal() {
 		double precio = super.precioFinal();
-		if(resolucion>40) {
-			precio+=precioBase*0.3;
+		// Si la resolución es superior a 40 se incrementa el precio
+		if (resolucion > 40) {
+			precio += precioBase * 0.3;
 		}
-		if(sintonizadorTDT) {
-			precio+=50;	
+		// Si tiene sintonizador TDT aumenta en 50 el precio
+		if (sintonizadorTDT) {
+			precio += 50;
 		}
 		return precio;
 	}
 
+	// --------------------Setters y Getters------------------------
 	public int getResolucion() {
 		return resolucion;
 	}
@@ -59,20 +67,13 @@ public class Television extends Electrodomestico{
 		this.sintonizadorTDT = sintonizadorTDT;
 	}
 
+	// Metodo toString
 	@Override
 	public String toString() {
-		return "TELEVISIÓN: "
-				+ "Resolucion: "+ resolucion
-				+ " / Sintonizador TDT: " + sintonizadorTDT 
-				+ " / Precio Base: "+ precioBase + "€"
-				+ " / Color: " + color 
-				+ " / Consumo Energetico: " + consumoEnergetico 
-				+ " / Peso: " + peso + "kg"
-				+ " / Precio final: " + precioFinal() + "€"
+		return "TELEVISIÓN: " + "Resolucion: " + resolucion + " / Sintonizador TDT: " + sintonizadorTDT
+				+ " / Precio Base: " + precioBase + "€" + " / Color: " + color + " / Consumo Energetico: "
+				+ consumoEnergetico + " / Peso: " + peso + "kg" + " / Precio final: " + precioFinal() + "€"
 				+ "\n----------------------";
 	}
-
-	
-
 
 }
